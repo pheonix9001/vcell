b2fd36e2a453682f6e444047242860ddd178bb93
pheonix9001 <pheonix9001@tutanota.com>
2023-10-10 17:28:15 +0530

    feat: Redefine cells *properly* + No more duplication of listeners(why is it called `merge`?) + Keep track of `cell`s instead of `function`s

--- src/main.lisp
+++ src/main.lisp
@@ -4,36 +4,49 @@
 
 (defclass cell ()
   ((val
-      :accessor cell-ref
+      :accessor cell-ref-noop
       :initarg :val)
    (subs
       :accessor cell-subs
-      :type (proper-list function)
-      :initform '()))
+      :type (proper-list cell)
+      :initform '())
+   (updater
+     :accessor cell-updater
+     :type function
+     :initarg :updater))
   (:documentation "A reactive cell"))
 
 ;; This is the magic
-(defparameter *cell-context* '()
-  "The function to calculate and set the current cell")
+(defparameter *cell-self* '()
+  "The current cell")
 
-(defmethod (cl:setf cell-ref) :after (val (cell cell))
-  (declare (ignore val))
-  (dolist (sub (cell-subs cell))
-     (funcall sub)))
-
-(defmethod cell-ref :after ((cell cell))
-  (when *cell-context*
+(defmethod cell-ref ((cell cell))
+  (when *cell-self*
     (setf (cell-subs cell)
-          (merge 'list
-                 (cell-subs cell)
-                 (list *cell-context*)
-                 #'eql))))
+          (remove-duplicates
+                 (cons *cell-self* (cell-subs cell))
+                 :test #'eql)))
+  (cell-ref-noop cell))
+
+(defun update-cell (cell)
+  "Update a cell to match its `updater`"
+  (let ((*cell-self* cell))
+    (setf (cell-ref-noop cell) (funcall (cell-updater cell)))
+    (dolist (sub (cell-subs cell))
+       (update-cell sub))))
+
+(defmacro cell-set (cell-code &body body)
+  "Set a cell to a value"
+  (let ((cell (gensym "cell")))
+    `(let ((,cell ,cell-code))
+       (setf (cell-updater ,cell) (lambda () ,@body))
+       (update-cell ,cell))))
 
 (defmacro cell (&body body)
   "Defines a cell"
   (let ((res (gensym "res-cell")))
-    `(let* ((,res (make-instance 'cell :val '()))
-            (*cell-context*
-              (lambda () (setf (cell-ref ,res) (progn ,@body)))))
-       (funcall *cell-context*)
+    `(let* ((,res (make-instance 'cell
+                    :val '()
+                    :updater (lambda () ,@body))))
+       (update-cell ,res)
        ,res)))
